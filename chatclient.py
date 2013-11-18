fc9f02bb791bf180d6ec299172a6cf9448480ac4
hoiming <haiming_liang@yahoo.com>
2013-11-18 18:31:07 +0800

    Update chatclient.py

--- chatclient.py
+++ chatclient.py
@@ -3,41 +3,55 @@
 import socket
 import threading
 import traceback
+import Queue
+import sys
 
+class ChatClient(object):
 
-host = 'localhost'
-port = 42345
-inString =''
-outString = ''
-def sendMessage(sock):
-    global outString
-    while 1:
-        try:
-            outString = raw_input('Input: ')
-            if outString:
-                sock.sendall(outString)
-        except:
-            traceback.print_exc()
-            sock.close()
+    def __init__(self):
+        '''
+        初始化
+        '''
+        self.host = 'localhost'
+        self.port = 32345
+        self.queue = Queue.Queue()
+        self.outString = ''
+        self.inString =''
+    def sendMessage(self,sock):
+        while 1:
+            try:
+                self.outString = raw_input('Input: ')
+                if self.outString:
+                    sock.sendall(self.outString)
+                    self.queue.put(self.outString)
+            except:
+                traceback.print_exc()
+                sock.close()
+                
 
-def receiveMessage(sock):
-    global inString
-    while 1:
-        try:
-            inString = sock.recv(1024)
-            if not inString:
+    def receiveMessage(self,sock):
+        while 1:
+            try:
+                self.inString = sock.recv(1024)
+                if not self.inString:
+                    raise
+                if self.inString != self.outString:
+                    print self.inString
+            except:
+                traceback.print_exc()
+                sock.close()
                 break
-            if outString != inString:
-                print inString
-        except:
-            traceback.print_exc()
-            sock.close()
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((host,port))
-sm  = threading.Thread(target = sendMessage, args =(sock,))
-sm.start()
-rm = threading.Thread(target = receiveMessage, args = (sock,))
-rm.start()
+    def run(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.host,self.port))
+        sm  = threading.Thread(target = self.sendMessage, args =(sock,))
+        sm.start()
+        rm = threading.Thread(target = self.receiveMessage, args = (sock,))
+        rm.start()
+        self.queue.put("")
 
+if __name__ == '__main__':
+    chatClient = ChatClient()
+    chatClient.run()
         
